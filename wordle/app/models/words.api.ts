e7802238fd733d6d3bada9a16d01506f54fd29ab
Daniel Martinez <danimartcas@hotmail.com>
2024-01-30 15:16:15 +0100

    feat - Add model to retrieve random word from words API

---
+++ wordle/app/models/words.api.ts
@@ -0,0 +1,25 @@
+import { Locale } from "~/types";
+
+type Word = string;
+
+export async function getRandomWord(
+  lang: Locale = Locale.EN,
+  length: number = 5,
+): Promise<Word> {
+  const response = await fetch(
+    `https://random-word-api.herokuapp.com/word?lang=${lang}&length=${length}`,
+  );
+
+  try {
+    const jsonResponse = await response.json();
+    const word = jsonResponse.pop();
+
+    if (typeof word === "undefined") {
+      throw new Error("API Server down!");
+    }
+
+    return word;
+  } catch (e) {
+    throw e;
+  }
+}
